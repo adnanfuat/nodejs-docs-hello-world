64c79eda4db0bc42f57b8eb868a4f14e3ed747ae
Tamer UZUN <yigitruzgaruzun@gmail.com>
2019-12-10 10:31:28 +0300

    destek2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hey Kader!!! Node.js çalışıyor. Üstelik GitHub desteğini de yanıma aldım");
+    response.end("Hey Kader!!! Node.js CALISIYOR. USTELIK GitHub destegini de yanıma aldım");
 });
 
 const port = process.env.PORT || 1337;
