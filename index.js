51e1029d45d1a71ed8a0749b5c0e15f917e6eec5
Tamer UZUN <yigitruzgaruzun@gmail.com>
2019-12-10 10:26:36 +0300

    KADERE NOT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello dear!!! GitHub Actions is cool!");
+    response.end("Hey Kader!!! Node.js çalışıyor. Üstelik GitHub desteğini de yanıma aldım");
 });
 
 const port = process.env.PORT || 1337;
